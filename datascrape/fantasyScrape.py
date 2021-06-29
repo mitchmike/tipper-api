5ffef32f8da2442a189eeaf952a3157b0ba09c66
mitchmike <michaeljm127@gmail.com>
2021-06-30 08:47:14 +1000

    tests for fantasy point scraper

--- datascrape/fantasyScrape.py
+++ datascrape/fantasyScrape.py
@@ -14,14 +14,10 @@ from datascrape.player import Player
 from datascrape.playerFantasy import PlayerFantasy
 from datascrape.playerSupercoach import PlayerSupercoach
 
-engine = create_engine('postgresql://postgres:oscar12!@localhost:5432/tiplos?gssencmode=disable')
-Base.metadata.create_all(engine, checkfirst=True)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
 
 def main():
+    engine = create_engine('postgresql://postgres:oscar12!@localhost:5432/tiplos?gssencmode=disable')
+    Base.metadata.create_all(engine, checkfirst=True)
     year = 2021
     # TODO: parameterise round / year / find current round based on date / call to footywire.
     for mode in ['dream_team', 'supercoach']:
@@ -31,22 +27,9 @@ def main():
             res = requests.get(url)
             soup = bs4.BeautifulSoup(res.text, 'html.parser')
             try:
-                header_row = soup.select('.bnorm')[0].parent
-                table = header_row.parent.contents
-                table = [x for x in table if x != '\n']
-                headers = []
-                for header in header_row.findAll('td'):
-                    text = header.text.strip()
-                    if re.match('^20.*Salary', text):
-                        headers.append('round_salary')
-                    elif re.match('.*Score', text):
-                        headers.append('round_score')
-                    elif re.match('.*Value', text):
-                        headers.append('round_value')
-                    else:
-                        headers.append(text)
+                table, headers = get_data_table(soup)
             except Exception as e:
-                print(f'Failed to grab data for {year} round {round}. Exception was: {e}')
+                print(f'Failed to parse data for {year} round {round}. Exception was: {e}')
                 exc_type, exc_obj, exc_tb = sys.exc_info()
                 fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
                 print(exc_type, fname, exc_tb.tb_lineno)
@@ -55,9 +38,27 @@ def main():
             print(f'Found {len(data_rows)} fantasy records for {year}, round {round}')
             fantasies = []
             for row in data_rows:
-                fantasies.append(populate_fantasy(mode, row, headers, year, round))
-            insert_fantasies(mode, fantasies, year, round)
-        
+                fantasies.append(populate_fantasy(mode, row, headers, year, round, engine))
+            insert_fantasies(mode, fantasies, year, round, engine)
+
+
+def get_data_table(soup):
+    header_row = soup.select('.bnorm')[0].parent
+    table = header_row.parent.contents
+    table = [x for x in table if x != '\n']
+    headers = []
+    for header in header_row.findAll('td'):
+        text = header.text.strip()
+        if re.match('^20.*Salary', text):
+            headers.append('round_salary')
+        elif re.match('.*Score', text):
+            headers.append('round_score')
+        elif re.match('.*Value', text):
+            headers.append('round_value')
+        else:
+            headers.append(text)
+    return table, headers
+
 
 def scrape_rows(table):
     rows = []
@@ -82,7 +83,8 @@ def scrape_rows(table):
     return rows
 
 
-def populate_fantasy(mode, fantasy_row, headers, fantasy_year, fantasy_round):
+def populate_fantasy(mode, fantasy_row, headers, fantasy_year, fantasy_round, engine):
+    Session = sessionmaker(bind=engine)
     fantasy = PlayerFantasy() if mode == 'dream_team' else PlayerSupercoach()
     fantasy.updated_at = datetime.datetime.now()
     fantasy.year = fantasy_year
@@ -97,11 +99,12 @@ def populate_fantasy(mode, fantasy_row, headers, fantasy_year, fantasy_round):
         key = headers[i].lower()
         value = fantasy_row[i]
         if key == 'player':
-            player = session.execute(select(Player).filter_by(team=team_name, name_key=value)).first()
-            if player:
-                fantasy.player_id = player[0].id
-            else:
-                print(f'no player for {fantasy_row}, {value}')
+            with Session() as session:
+                player = session.execute(select(Player).filter_by(team=team_name, name_key=value)).first()
+                if player:
+                    fantasy.player_id = player[0].id
+                else:
+                    print(f'no player for {fantasy_row}, {value}')
         elif key == 'rank':
             fantasy.round_ranking = value
         elif key == 'round_salary':
@@ -113,28 +116,30 @@ def populate_fantasy(mode, fantasy_row, headers, fantasy_year, fantasy_round):
     return fantasy
 
 
-def insert_fantasies(mode, fantasies, fantasy_year, fantasy_round):
-    fantasies_persisted = session.execute(select(PlayerFantasy if mode == 'dream_team' else PlayerSupercoach)
+def insert_fantasies(mode, fantasies, fantasy_year, fantasy_round, engine):
+    Session = sessionmaker(bind=engine)
+    with Session() as session:
+        fantasies_persisted = session.execute(select(PlayerFantasy if mode == 'dream_team' else PlayerSupercoach)
                                           .filter_by(year=fantasy_year, round=fantasy_round)).all()
-    print(f'{len(fantasies_persisted)} Records already found in DB for {fantasy_year}, round {fantasy_round}')
-    for fantasy in fantasies:
-        if fantasy.player_id is None:
-            print(f'Record is missing details required for persistance (player_id). doing nothing. Record: {fantasy}')
-            continue
-        db_match = [x[0] for x in fantasies_persisted
-                    if fantasy.player_id == x[0].player_id
-                    and fantasy.round == x[0].round
-                    and fantasy.year == x[0].year]
-        # just add the id to our obj, then merge, then commit session
-        if db_match:
-            fantasy.id = db_match[0].id
-            continue  # do nothing
-        session.add(fantasy)  # add only if new
-    try:
-        session.commit()
-    except Exception as e:
-        print(f'Could not commit fantasy: {fantasies} due to exception: {e} \n Rolling back')
-        session.rollback()
+        print(f'{len(fantasies_persisted)} Records already found in DB for {fantasy_year}, round {fantasy_round}')
+        for fantasy in fantasies:
+            if fantasy.player_id is None:
+                print(f'Record is missing details required for persistance (player_id). doing nothing. Record: {fantasy}')
+                continue
+            db_match = [x[0] for x in fantasies_persisted
+                        if fantasy.player_id == x[0].player_id
+                        and fantasy.round == x[0].round
+                        and fantasy.year == x[0].year]
+            # just add the id to our obj, then merge, then commit session
+            if db_match:
+                fantasy.id = db_match[0].id
+                continue  # do nothing
+            session.add(fantasy)  # add only if new
+        try:
+            session.commit()
+        except Exception as e:
+            print(f'Could not commit fantasy: {fantasies} due to exception: {e} \n Rolling back')
+            session.rollback()
 
 
 if __name__ == '__main__':
---
+++ test/test_fantasyScrape.py
@@ -0,0 +1,181 @@
+import datetime
+import unittest
+import bs4
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from datascrape import fantasyScrape
+from datascrape import base
+from datascrape.player import Player
+from datascrape.playerFantasy import PlayerFantasy
+
+
+class TestFantasyScrape(unittest.TestCase):
+    # for testing the entire html document
+    HTML_SOURCE_FILE = '2021-r1-fantasy-points.html'
+    DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+    HTML_SOURCE_FILE = os.path.join(DIR_PATH, 'html_files', HTML_SOURCE_FILE)
+
+    # for testing an individual row
+    HTML_ONE_ROW = '''<table width="688" border="0" cellspacing="0" cellpadding="0">
+                <tr>
+                <td class="bnorm" width="60">Rank</td>
+                <td height="44" class="lbnorm">Player</td>
+                <td class="lbnorm" width="90">Team</td>
+                <td class="bnorm" width="90">Current<br/>Salary</td>
+                <td class="bnorm" width="90"><a href="dream_team_round?p=&s=S">2021 R1<br/>Salary</a></td>
+                <td class="bnorm" width="90"><a href="dream_team_round?p=&s=T">2021 R1<br/>Score</a></td>
+                <td class="bnorm" width="90"><a href="dream_team_round?p=&s=V">*2021 R1<br/>Value</a></td>
+                </tr>
+                <tr bgcolor="#f2f4f7" onMouseOver="this.bgColor='#cbcdd0';" onMouseOut="this.bgColor='#f2f4f7';">
+                <td height="24" align="center">1</td>
+                <td align="left"><a href="pr-essendon-bombers--andrew-mcgrath">Andrew McGrath</a>
+                 <i>Injured</i>
+                </td>
+                <td align="left"><a href="th-essendon-bombers?year=2021">Bombers</a></td>
+                <td align="center">$607,000</td>
+                <td align="center">$738,000</td>
+                <td align="center">141</td>
+                <td align="center">19.1</td>
+                </tr>
+                </table>'''
+
+    @classmethod
+    def setUpClass(cls):
+        TestFantasyScrape._engine = create_engine(
+            'postgresql://postgres:oscar12!@localhost:5432/tiplos-test?gssencmode=disable'
+        )
+        base.Base.metadata.create_all(TestFantasyScrape._engine, checkfirst=True)
+        TestFantasyScrape.Session = sessionmaker(bind=TestFantasyScrape._engine)
+
+    def setUp(self):
+        with TestFantasyScrape.Session() as cleanup_session:
+            cleanup_session.query(PlayerFantasy).delete()
+            cleanup_session.query(Player).delete()
+            cleanup_session.execute("ALTER SEQUENCE player_id_seq RESTART WITH 1")
+            cleanup_session.execute("ALTER SEQUENCE fantasy_id_seq RESTART WITH 1")
+            cleanup_session.commit()
+
+        # add player to put fantasy against
+        add_player_to_db(1, 'andrew-mcgrath', 'essendon-bombers')
+
+        # Set up one-row table
+        one_row_soup = bs4.BeautifulSoup(self.HTML_ONE_ROW, 'html.parser')
+        self.one_row_table, self.one_row_headers = fantasyScrape.get_data_table(one_row_soup)
+        self.one_data_row = fantasyScrape.scrape_rows(self.one_row_table)[0]
+        self.one_fantasy = fantasyScrape.populate_fantasy('dream_team', self.one_data_row,
+                                                          self.one_row_headers, 2021, 1, TestFantasyScrape._engine)
+
+        # Stub html file used - to refresh file run get_html.py with 'players' as first arg
+        with open(self.HTML_SOURCE_FILE, 'r') as file:
+            soup = bs4.BeautifulSoup(file.read(), 'html.parser')
+        self.table, self.headers = fantasyScrape.get_data_table(soup)
+        self.data_rows = fantasyScrape.scrape_rows(self.table)
+
+    def test_scrape_row(self):
+        row = fantasyScrape.scrape_rows(self.one_row_table)
+        data_row = row[0]
+        self.assertEqual(len(data_row), 7)
+        self.assertEqual(data_row[0], '1')
+        self.assertEqual(data_row[1], 'andrew-mcgrath')
+        self.assertEqual(data_row[2], 'essendon-bombers')
+        self.assertEqual(data_row[3], '$607,000')
+        self.assertEqual(data_row[4], '$738,000')
+        self.assertEqual(data_row[5], '141')
+        self.assertEqual(data_row[6], '19.1')
+
+    def test_scrape_row_multiple(self):
+        data_rows = fantasyScrape.scrape_rows(self.table)
+        self.assertEqual(len(data_rows), 404)
+        data_row = data_rows[0]
+        self.assertEqual(data_row[0], '1')
+        self.assertEqual(data_row[1], 'andrew-mcgrath')
+        self.assertEqual(data_row[2], 'essendon-bombers')
+        self.assertEqual(data_row[3], '$607,000')
+        self.assertEqual(data_row[4], '$738,000')
+        self.assertEqual(data_row[5], '141')
+        self.assertEqual(data_row[6], '19.1')
+        data_row_last = data_rows[-1]
+        self.assertEqual(data_row_last[0], '404')
+        self.assertEqual(data_row_last[1], 'alex-pearce')
+        self.assertEqual(data_row_last[2], 'fremantle-dockers')
+        self.assertEqual(data_row_last[3], '$217,000')
+        self.assertEqual(data_row_last[4], '$232,000')
+        self.assertEqual(data_row_last[5], '7')
+        self.assertEqual(data_row_last[6], '3.0')
+
+    def test_populate_fantasy_player_doesnt_exist(self):
+        self.one_data_row[1] = 'NO_NAME'
+        fantasy = fantasyScrape.populate_fantasy('dream_team', self.one_data_row, self.one_row_headers,
+                                                 2021, 1, TestFantasyScrape._engine)
+        self.assertIsNotNone(fantasy)
+        self.assertEqual(fantasy.player_id, None)
+        self.assertEqual(fantasy.year, 2021)
+        self.assertEqual(fantasy.round, 1)
+        self.assertEqual(fantasy.round_ranking, '1')
+        self.assertEqual(fantasy.round_salary, '$738,000')
+        self.assertEqual(fantasy.round_score, '141')
+        self.assertEqual(fantasy.round_value, '19.1')
+
+    def test_populate_fantasy_player_exists(self):
+        fantasy = fantasyScrape.populate_fantasy('dream_team', self.one_data_row, self.one_row_headers,
+                                                 2021, 1, TestFantasyScrape._engine)
+        self.assertIsNotNone(fantasy)
+        self.assertEqual(fantasy.player_id, 1)
+        self.assertEqual(fantasy.year, 2021)
+        self.assertEqual(fantasy.round, 1)
+        self.assertEqual(fantasy.round_ranking, '1')
+        self.assertEqual(fantasy.round_salary, '$738,000')
+        self.assertEqual(fantasy.round_score, '141')
+        self.assertEqual(fantasy.round_value, '19.1')
+
+    def test_insert_fantasies_already_exists(self):
+        None
+
+    def test_insert_fantasies_no_player_id_in_fantasy(self):
+        None
+
+    def test_insert_fantasies_new_fantasy(self):
+        fantasyScrape.insert_fantasies('dream_team', [self.one_fantasy], 2021, 1, TestFantasyScrape._engine)
+        with TestFantasyScrape.Session() as session:
+            fantasy_persisted = session.query(PlayerFantasy).all()
+        self.assertEqual(len(fantasy_persisted), 1)
+        fantasy = fantasy_persisted[0]
+        self.assertIsNotNone(fantasy)
+        self.assertEqual(fantasy.player_id, 1)
+        self.assertEqual(fantasy.year, 2021)
+        self.assertEqual(fantasy.round, 1)
+        self.assertEqual(fantasy.round_ranking, 1)
+        self.assertEqual(fantasy.round_salary, '$738,000.00')
+        self.assertEqual(fantasy.round_score, 141)
+        self.assertEqual(fantasy.round_value, 19.1)
+
+    def test_insert_fantasies_multiple(self):
+        None
+
+    def tearDown(self):
+        with TestFantasyScrape.Session() as cleanup_session:
+            cleanup_session.query(PlayerFantasy).delete()
+            cleanup_session.query(Player).delete()
+            cleanup_session.commit()
+
+    @classmethod
+    def tearDownClass(cls):
+        base.Base.metadata.drop_all(TestFantasyScrape._engine, checkfirst=False)
+
+
+def add_player_to_db(player_id, name, team):
+    # add player to put injury against
+    player = Player()
+    player.name_key = name
+    player.id = player_id
+    player.DOB = datetime.date(1990, 1, 1)
+    player.team = team
+    with TestFantasyScrape.Session() as player_session:
+        player_session.add(player)
+        player_session.commit()
+
+
+if __name__ == '__main__':
+    unittest.main()
