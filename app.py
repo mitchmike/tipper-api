bdb8b1f23efee30940a71e371381bdec76ef4721
mitchmike <michaeljm127@gmail.com>
2021-12-11 21:50:49 +1100

    config

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
+
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    secret_key = app.config.get("SECRET_KEY")
+    APP_SETTINGS = app.config.get("APP_SETTINGS")
+    return f"The configured secret key is {secret_key}, {APP_SETTINGS}."
---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+
+class Config:
+    ENV_NAME = 'NONE'
+    SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
+
+
+class ProductionConfig(Config):
+    ENV_NAME = 'PROD'
+    SECRET_KEY = 'prod-key'
+
+
+class StagingConfig(Config):
+    ENV_NAME = 'STAGING'
+    SECRET_KEY = 'staging-key'
+
+
+class DevelopmentConfig(Config):
+    ENV_NAME = 'DEV'
+    SECRET_KEY = 'dev-key'
